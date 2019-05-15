28220c89dbeb4ba164e131fa231ec8a3f41f969b
Kanokkorn <kanokorn@outlook.jp>
2019-05-15 16:02:11 +0700

    Time with keyboard interrupt detection

--- pyscript/clock.py
+++ pyscript/clock.py
@@ -1,3 +1,13 @@
-import time 
-while 1:
-    print(str(time.asctime(time.localtime(time.time()))), end='\r')
+import time
+
+
+def display_time():
+    return print(str(time.asctime(time.localtime(time.time()))), end="\r")
+
+
+if __name__ == "__main__":
+    try:
+        while True:
+            display_time()
+    except KeyboardInterrupt as err:
+        print("Keyboard Interrupt... Stopped")
