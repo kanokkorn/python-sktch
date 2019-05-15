3010b5a277e41466dccdac31ef2f4135fd1d6d64
Kanokkorn <kanokorn@outlook.jp>
2019-05-15 15:48:34 +0700

    Greeting !

--- pyscript/Hello.py
+++ pyscript/Hello.py
@@ -1,13 +1,7 @@
-import cv2
-import numpy as np
-from matplotlib import pyplot as plt
+def greeting(name):
+    return print("Hello " + str(name) + "!")
 
-img = cv2.imread('img/shyaro.jpg',0)
-edges = cv2.Canny(img,200,400)
 
-plt.subplot(121),plt.imshow(img,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-
-plt.show()
+if __name__ == "__main__":
+    name = str(input("What's your name? : "))
+    greeting(name)
