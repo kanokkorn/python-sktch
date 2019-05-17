fb28ed5c0647e954c02e2a5e707c74bdcdc60d9d
Kanokkorn <kanokorn@outlook.jp>
2019-05-17 15:31:23 +0700

    format document

--- pyscript/validate_gen.py
+++ pyscript/validate_gen.py
@@ -2,14 +2,14 @@ import glob, os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-current_dir = './img/'
+current_dir = "./img"
 
 percentage = 20
 
 train_file = open("train.txt", "w")
 test_file = open("test.txt", "w")
 
-counter = 1 
+counter = 1
 index = round(100 / percentage)
 for _path_ in glob.iglob(os.path.join(current_dir, "*.jpg")):
     title, ext = os.path.splitext(os.path.basename(_path_))
