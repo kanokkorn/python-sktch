865606db552edf2dbea6ff71e272d57712a00b49
Kanokkorn <kanokorn@outlook.jp>
2019-05-22 23:55:09 +0700

    get CSV value

---
+++ pyscript/csv_value.py
@@ -0,0 +1,10 @@
+import csv
+
+with open("pyscript/demo.csv", newline="") as csv_file:
+    csv_data = csv.reader(csv_file, delimiter=",")
+    for row in csv_data:
+        try:
+            data_row = row[0]
+            print(data_row)
+        except IndexError:
+            exit()
