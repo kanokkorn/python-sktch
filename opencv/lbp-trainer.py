31c9a9edbe9c07edc1cc92fc6ea869f91c3dae5a
kanokkorn <kanokorn@outlook.jp>
2019-08-19 17:48:43 +0700

    fix bug and typo

--- opencv/lbp-trainer.py
+++ opencv/lbp-trainer.py
@@ -4,7 +4,7 @@ import argparse
 import numpy as np
 from PIL import Image
 
-def get_img_id():
+def get_img_id(path):
         
     image_path = (os.path.join(path, f) for f in os.listdir(path))
     faces = []
@@ -12,10 +12,10 @@ def get_img_id():
     for image_path in image_path:
 	    face_img = Image.open(image_path).convert("L")
 	    face_np = np.array(face_img, "uint8")
-	    ID = int(os.path.split(image_path)[-1].split(",")[1])
-	    faces.append(face_img)
+	    ID = int(os.path.split(image_path)[-1].split(".")[1])
+	    faces.append(face_np)
 	    print(ID)
-	    ids.append(face_np)
+	    ids.append(ID)
 	    cv2.imshow("training", face_np)
 	    q = cv2.waitKey(1) and 0xff
 	    if q == 27:
@@ -24,7 +24,7 @@ def get_img_id():
 
 if __name__ == "__main__":
     
-    recognize = cv2.face.LBPHFaceRecognizer_create()
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
     
     arg = argparse.ArgumentParser(description="Train LBP Classifier")
     arg.add_argument("--path", "--p", metavar="path", type=str, help="Path to folder you want to train")
@@ -36,6 +36,6 @@ if __name__ == "__main__":
     
     face_id, faces = get_img_id(path)
     recognizer.train(faces, np.array(face_id))
-    recohnizer.save(out)
+    recognizer.save(out)
     cv2.destroyAllWindows()
     
