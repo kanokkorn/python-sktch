a9ccc2f1ffb5344f9454e9c4618b388831e4c26d
kanokkorn <kanokorn@outlook.jp>
2019-09-25 20:49:25 +0700

    change to local path

--- opencv/face_detection.py
+++ opencv/face_detection.py
@@ -3,11 +3,12 @@ import os
 import time
 import sys
 
-face_cascade = cv2.CascadeClassifier("C:\\Users\\kanok\\Documents\\lbpcascade_animeface.xml")
+face_cascade = cv2.CascadeClassifier("path to .xml files")
 cv2.ocl.setUseOpenCL(1)
-cap = cv2.VideoCapture("D:\\backup-usb\\test_vid\\gochuumon2_9.mp4")
+cap = cv2.VideoCapture(0)
 num = 0
-while 1:
+
+while True:
     ret, img1 = cap.read()
     img = cv2.resize(img1, (0, 0), fx=1, fy=1)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -17,14 +18,11 @@ while 1:
         cv2.rectangle(img, (x, y), (x + w, y + h), (175, 244, 65), 1)
         roi_gray = gray[y : y + h, x : x + w]
         roi_color = img[y : y + h, x : x + w]
-
-        #cv2.imwrite('C:\\Users\\kanok\\Documents\\python-sktch\\opencv\\image_save\\image_'+str(num)+'.jpg', roi_color) #save img to folder
         cv2.imshow("face-output", roi_color)
         num += 1
-    cv2.imshow("gochiusa_opencv", img)
+    cv2.imshow("output", img)
     k = cv2.waitKey(1) & 0xFF
     if k == 27:
         break
     cap.release()
     cv2.destroyAllWindows()
-
