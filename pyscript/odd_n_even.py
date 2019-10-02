f4fd9b2761b891c5adcb172ce6ac45015269ef0e
kanokkorn <kanokorn@outlook.jp>
2019-10-02 13:23:23 +0700

    remove shebang

--- pyscript/odd_n_even.py
+++ pyscript/odd_n_even.py
@@ -1,11 +1,7 @@
-#!/usr/bin/python3
-
 def odd_even(x):
-        if (x % 2) == 0:
-                return print("This is even number")
-        return print("This is odd number")
+    if (x % 2) == 0:
+        return print("This is even number")
+    return print("This is odd number")
 
 if __name__ == "__main__":
-
-        odd_even(int(input("Enter number : ")))
-        
+    odd_even(int(input("Enter number : ")))
