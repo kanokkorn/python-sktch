7fa83240c06867eb1e8c7ef5e303cd3fdfd303bd
kanokkorn <kanokorn@outlook.jp>
2019-10-28 19:49:55 +0700

    read config from JSON

--- TensorFlow/image-classify.py
+++ TensorFlow/image-classify.py
@@ -5,6 +5,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
+import json
 import os
 import numpy
 import matplotlib.pyplot as plt
@@ -20,4 +21,16 @@ def get_dirname(dirlist):
     for x in range(len(dirlist)+1):
       train_path = os.path.join(train_dir, str(dirlist[x]))
 
+# read config from JSON 
+def read_config(filename):
+  with open('config.json', 'r') as config:
+    config_data = config.read()
+  obj = json.loads(data)
+  batch = int(obj['batch'])
+  epochs = int(obj['epochs'])
+  width = int(obj['width'])
+  height = int(obj['height'])
+
+# preparation
+def prepare():
 
