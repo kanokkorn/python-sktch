6c79d305b9b88e78f7f60dcb639abfdffff1339a
kanokkorn <kanokorn@outlook.jp>
2019-10-31 18:41:05 +0700

    checking cuda

--- pytorch/torch_object_detect.py
+++ pytorch/torch_object_detect.py
@@ -1,3 +1,8 @@
 import torchvision.models as models
 
 gochi = models.mobilenet_v2(pretrained=True, progress=True)
+
+if not args.disable_cuda and torch.cuda.is_available():
+  args.device() = torch.device('cuda')
+else:
+  args.device() = torch.device('cpu')
