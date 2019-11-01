f0412cf17279ebbcdc260547cf8cc6087351acc0
kanokkorn <kanokorn@outlook.jp>
2019-11-01 21:23:53 +0700

    use pathlib

--- TensorFlow/image-classify.py
+++ TensorFlow/image-classify.py
@@ -5,6 +5,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
+import pathlib
 import json
 import os
 import numpy
