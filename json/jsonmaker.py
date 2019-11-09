c51979104da372f18ea61ac5c47823596e2a9af6
kanokkorn <kanokorn@outlook.jp>
2019-11-09 10:39:28 +0700

    create basic JSON file

--- json/jsonmaker.py
+++ json/jsonmaker.py
@@ -1,7 +1,17 @@
 import json
 
 def main():
-  pass
+  agent_id_1 = {
+    'agent_1': {
+      'name': 'yukimori ai',
+      'trait': 'supportive'
+    }
+  }
+  json_write(agent_id_1)
 
-if __name__ = '__main__'"
+def json_write(data):
+  with open('data.json', 'w') as write:
+    json.dump(data, write)
+
+if __name__ == '__main__':
   main()
