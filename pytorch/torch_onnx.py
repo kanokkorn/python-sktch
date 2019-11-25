76e6a58d7f1b17fc2de781a2a051e49f8e0b9e3d
kanokkorn <kanokorn@outlook.jp>
2019-11-25 19:51:59 +0700

    extract weights to ONNX format

---
+++ pytorch/torch_onnx.py
@@ -0,0 +1,8 @@
+from caffe2.proto import caffe2_pb2
+from caffe2.python import core, net_drawer, net_printer, visualize, workspacem utils
+import numpy as np
+import os
+import subprocess
+from matplotlib import pyplot
+from skimage import io, transform
+
