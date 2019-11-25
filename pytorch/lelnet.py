947c8f03774bbaae5d1d3ade0037a72fc355c511
kanokkorn <kanokorn@outlook.jp>
2019-11-25 19:53:20 +0700

    lenet5 implement

---
+++ pytorch/lelnet.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datesets, models, transforms
+
+def nn():
+  pass
+
+if __name__ == '__main__':
+  pass
