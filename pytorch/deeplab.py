43bb3eac00a505153f7e0445d31f709e26530110
kanokkorn <kanokorn@outlook.jp>
2019-12-09 02:32:49 +0700

    add get image function

--- pytorch/deeplab.py
+++ pytorch/deeplab.py
@@ -1,12 +1,12 @@
-from torchvision import transform
+from torchvision import transforms
 from PIL import Image
 import torch
-model = torch.hub.load('pytorch/vision:0.4.2', 'deeplabv3_resnet101', pretrained=True)
+model = torch.hub.load('pytorch/vision:v0.4.2', 'deeplabv3_resnet101', pretrained=True)
 model.eval()
 
 def detect(filename):
   input_img = Image.open(filename)
-  preprocess = transform.Compose([
+  preprocess = transforms.Compose([
       transforms.ToTensor(),
       trnasforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
       ])
@@ -16,6 +16,12 @@ def detect(filename):
   with torch.no_grad():
     output = model(input_batch)['out'][0]
   output_predict = output.argmax(0)
+
+def get_img(url):
+  import urllib
+  url, file_name = (url, 'image.jpg')
+  try: urllib.URLopener().retrieve(url, file_name)
+  except: urllib.request.urlretrieve(url, file_name)
 
 if __name__ == '__main__':
   detect('path')
