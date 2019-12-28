b73f20d6fcdf6b15fbb9554f890ea434a07499eb
kanokkorn <kanokorn@outlook.jp>
2019-12-28 21:19:05 +0700

    fix typo

--- opencv/video_edge.py
+++ opencv/video_edge.py
@@ -5,5 +5,10 @@ cap = cv2.VideoCapture('./gochuumon2_9.mp4') # change video path
 
 while 1:
   _, img = cap.read()
-  if _ is 'false' print('Video not available')
   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  edge = cv2.Canny(gray, 75, 125)
+  cv2.imshow('edge', edge)
+  if cv2.waitKey(1) == ord('q'):
+    break
+cap.release()
+cv2.destroyAllWindow()
