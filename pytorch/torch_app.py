69687c5e54a806a8592c024a4f7bbbf777dff699
kanokkorn <kanokorn@outlook.jp>
2020-01-13 22:50:56 +0700

    refactoring

--- pytorch/torch_app.py
+++ pytorch/torch_app.py
@@ -3,16 +3,20 @@ import torchvision
 from torchvision import transforms
 from PIL import Image
 
+
+model = model.load_state_dict(torch.load('gochi-trained.pth'))
+model.eval()
+transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(
+      mean=[0.485, 0.456, 0.406],
+      std=[0.229, 0.224, 0.225])
+    ])
+
 def predict(path):
   img = Image.open(path)
-  transform = transforms.Compose([
-      transforms.Resize(256),
-      transforms.CenterCrop(224),
-      transforms.ToTensor(),
-      transforms.Normalize(
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225])
-      ])
   img_trans = transform(img)
   if torch.cuda.is_available():
     img_tensor = img_trans.view(1, 3, 224, 224).cuda
@@ -20,8 +24,6 @@ def predict(path):
     img_tensor = img_trans.view(1, 3, 224, 224).cuda
   
   with torch.no_grad():
-    model = model.load_state_dict(torch.load('gochi-trained.pth'))
-    model.eval()
     out = model(img_tensor)
     pred = torch.exp(out)
     top_k, top_class = pred.top_k(1, dim=1)
