3e8b06959ab3b6b421ca9f447df7251be9c6fab7
kanokkorn <kanokorn@outlook.jp>
2020-01-13 22:28:03 +0700

    add torch.no_grad

--- pytorch/torch_app.py
+++ pytorch/torch_app.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 def predict(path):
   img = Image.open(path)
-  transforms = transforms.Compose([
+  transform = transforms.Compose([
       transforms.Resize(256),
       transforms.CenterCrop(224),
       transforms.ToTensor(),
@@ -13,12 +13,19 @@ def predict(path):
         mean=[0.485, 0.456, 0.406],
         std=[0.229, 0.224, 0.225])
       ])
-  model = model.load_state_dict(torch.load('gochi-trained.pth'))
-  model.eval()
-  img_trans = transforms(img)
-  batch_trans = torch.unsqueeze(img_trans, 0)
-  out = model(batch_trans)
+  img_trans = transform(img)
+  if torch.cuda.is_available():
+    img_tensor = img_trans.view(1, 3, 224, 224).cuda
+  else:
+    img_tensor = img_trans.view(1, 3, 224, 224).cuda
   
+  with torch.no_grad():
+    model = model.load_state_dict(torch.load('gochi-trained.pth'))
+    model.eval()
+    out = model(img_tensor)
+    pred = torch.exp(out)
+    top_k, top_class = pred.top_k(1, dim=1)
+    print('Output: ', idx_to_class[top_class.cpu().numpy()[0][0]])
 
 if __name__ == '__main__':
   predict('../img/shyaro.jpg')
