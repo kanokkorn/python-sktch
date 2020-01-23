33b420fd9074e92a3e0b13ab3a30d7643064882d
kanokkorn <kanokorn@outlook.jp>
2020-01-23 09:58:29 +0700

    formatting

--- pytorch/torch_onnx.py
+++ pytorch/torch_onnx.py
@@ -1,10 +1,22 @@
 import torch
 import torchvision
+from torchvision import models
 
+def convert_onnx(path):
+  model = torch.load(path)
+  model.load_state_dict(model)
+  input_names = ['actual_input_1']+['learned_%d' % i for i in range(16)]
+  if torch.cuda.is_available():
+    mock_input  = torch.rand(10, 3, 224, 224, device='cuda')
+  else:
+    mock_input  = torch.rand(10, 3, 224, 224, device='cpu')
+  output_names = ['output']
+  torch.onnx.export(
+      model, mock_input,'output.onnx', 
+      verbose=True, 
+      input_names=input_names,
+      output_names=output_names
+      )
 
-dummy_input = torch.randn(10, 3, 224, 224, device='cpu')
-model = torchvision.models.alexnet(pretrained=True)
-
-input_names = ['actual_input_1']+['learned_%d' % i for i in range(16)]
-output_names = ['output_1']
-torch.onnx.export(model, dummy_input, 'alexnet.onnx', verbose=True, input_names=input_names, output_names=output_names)
+if __name__ == '__main__':
+  convert_onnx(input('input path: '))
