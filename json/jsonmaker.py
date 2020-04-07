b03816905df8effd8ddfd26305dbdab42ac28b64
kanokkorn <kanokorn@outlook.jp>
2020-04-08 04:33:44 +0700

    make json with dict

--- json/jsonmaker.py
+++ json/jsonmaker.py
@@ -1,11 +1,14 @@
 import json
 
-def main():
-  json_write()
+def getdata():
+  user = str(input('username:'))
+  data = str(input('data:'))
+  payload = {'username':user, 'data':data}
+  return payload
 
 def json_write(data):
   with open('data.json', 'w') as write:
     json.dump(data, write)
 
 if __name__ == '__main__':
-  main()
+  print(getdata())
