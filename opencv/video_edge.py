ae29bd789ce1ed1b174c971d2b2615e021c7a2d9
kanokkorn <kanokorn@outlook.jp>
2020-05-13 21:09:10 +0700

    maybe work, maybe not

--- opencv/video_edge.py
+++ opencv/video_edge.py
@@ -1,14 +1,30 @@
 import cv2
+import argparse
 
-cv2.ocl.setUseOpenCL(1)
-cap = cv2.VideoCapture('./gochuumon2_9.mp4') # change video path
+parser = argparse.ArgumentParser(
+    description='display canny of video'
+    )
+parser.add_argument(
+    '--input', '-i', help='path to video',
+    type=str, required=True
+    )
+args = vars(parser.parse_args())
 
-while 1:
-  _, img = cap.read()
-  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-  edge = cv2.Canny(gray, 75, 125)
-  cv2.imshow('edge', edge)
-  if cv2.waitKey(1) == ord('q'):
-    break
-cap.release()
-cv2.destroyAllWindows()
+def vid_canny(video_path):
+  capture = cv2.VideoCapture(video_path)
+  if capture.isOpened is False:
+    print('cant open video')
+  while capture.isOpened():
+    ret, img = capture.read()
+    if ret is not True:
+      break
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    canny = cv2.Canny(gray, 200, 400)
+    cv2.imshow('edged', canny)
+    if cv2.waitKey(1) == ord('q'):
+      break
+    capture.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+  vid_canny(args['input'])
