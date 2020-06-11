d069733cae1ef37128bdb24e59344061a8bc05ea
kanokkorn <kanokorn@outlook.jp>
2020-06-11 21:08:31 +0700

    update kalman filter

--- matplotlib/kalman_filter.py
+++ matplotlib/kalman_filter.py
@@ -1,65 +1,11 @@
-# Kalman filter example demo in Python
+import math
+import random
+from matplotlib import pyplot
 
-# A Python implementation of the example given in pages 11-15 of "An
-# Introduction to the Kalman Filter" by Greg Welch and Gary Bishop,
-# University of North Carolina at Chapel Hill, Department of Computer
-# Science, TR 95-041,
-# https://www.cs.unc.edu/~welch/media/pdf/kalman_intro.pdf
+est_err, mea, minus_est_err, mea_err = 0
 
-# by Andrew D. Straw
+gain = lambda est_err, mea_err: est_err/(mea_err+est_err) 
 
-import numpy as np
-import matplotlib.pyplot as plt
+est = lambda minus_est, mea, gain: (minus_est+gain)*(mea-minus_est) 
 
-plt.rcParams["figure.figsize"] = (10, 8)
-
-# intial parameters
-n_iter = 50
-sz = (n_iter,)  # size of array
-x = -0.37727  # truth value (typo in example at top of p. 13 calls this z)
-z = np.random.normal(x, 0.1, size=sz)  # observations (normal about x, sigma=0.1)
-
-Q = 1e-5  # process variance
-
-# allocate space for arrays
-xhat = np.zeros(sz)  # a posteri estimate of x
-P = np.zeros(sz)  # a posteri error estimate
-xhatminus = np.zeros(sz)  # a priori estimate of x
-Pminus = np.zeros(sz)  # a priori error estimate
-K = np.zeros(sz)  # gain or blending factor
-
-R = 0.1 ** 2  # estimate of measurement variance, change to see effect
-
-# intial guesses
-xhat[0] = 0.0
-P[0] = 1.0
-
-for k in range(1, n_iter):
-    # time update
-    xhatminus[k] = xhat[k - 1]
-    Pminus[k] = P[k - 1] + Q
-
-    # measurement update
-    K[k] = Pminus[k] / (Pminus[k] + R)
-    xhat[k] = xhatminus[k] + K[k] * (z[k] - xhatminus[k])
-    P[k] = (1 - K[k]) * Pminus[k]
-
-plt.figure()
-plt.plot(z, "k+", label="noisy measurements")
-plt.plot(xhat, "b-", label="a posteri estimate")
-plt.axhline(x, color="g", label="truth value")
-plt.legend()
-plt.title("Estimate vs. iteration step", fontweight="bold")
-plt.xlabel("Iteration")
-plt.ylabel("Voltage")
-
-plt.figure()
-valid_iter = range(1, n_iter)  # Pminus not valid at step 0
-plt.plot(valid_iter, Pminus[valid_iter], label="a priori error estimate")
-plt.title(
-    "Estimated $\it{\mathbf{a \ priori}}$ error vs. iteration step", fontweight="bold"
-)
-plt.xlabel("Iteration")
-plt.ylabel("$(Voltage)^2$")
-plt.setp(plt.gca(), "ylim", [0, 0.01])
-plt.show()
+est_err = lambda gain, minus_est_err: (1-gain)*minus_est_err 
