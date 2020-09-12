ffdd264b6b604734d1d0cb5d80b21bf433b002ae
kanokkorn <kanokorn@outlook.jp>
2020-09-12 15:15:52 +0700

    messing around with audio

--- pytorch/torch_audio.py
+++ pytorch/torch_audio.py
@@ -1,7 +1,12 @@
 import torch
 import torchaudio
+import matplotlib.pyplot as plt
 
 torchaudio.set_audio_backend('soundfile')
+waveform, sample_rate = torchaudio.load('data/Clover.flac')
+print(f'Shape of waveform: {waveform.size()}')
+print(f'Sample rate of waveform: {sample_rate}')
 
-waveform, sample_rate = torchaudio.load('.wav')
-torchaudio.save('_save.wav', waveform, sample_rate)
+plt.figure()
+plt.plot(waveform.t().numpy())
+plt.show()
