3b1f2cb2ee378baa2eefa6467e3e76cd00b91bf0
kanokkorn <kanokorn@outlook.jp>
2020-09-12 15:13:10 +0700

    use dict and list

--- opencv/detect_w_lbp.py
+++ opencv/detect_w_lbp.py
@@ -1,52 +1,39 @@
-import os
-import time
 import cv2
 
+#haar = cv2.CascadeClassifier("path to .xml")
+#reg.read("path to .yml")
+haar = cv2.CascadeClassifier("./animeface.xml")
+cap = cv2.VideoCapture("./test.mp4")
+reg = cv2.face.LBPHFaceRecognizer_create() 
+reg.read("./gochi.yml")
+
+face_name = {'Chino':(244, 196, 65), 
+              'Cocoa':(65, 128, 244), 
+              'Rize':(244, 65, 143),
+              'Sharo':(65, 238, 244),
+              'Chiya':(65, 244, 71)
+}
+
 def detect_w_lbp():
-  cv2.ocl.setUseOpenCL(1)
-  haar = cv2.CascadeClassifier("path to .xml")
-  cap = cv2.VideoCapture("video")
-  reg = cv2.face.LBPHFaceRecognizer_create() 
-  reg.read("path to .yml")
-       
-  while True:
-    face_id = 0
+  while cap.isOpened():
     ret, img = cap.read()
+    if not ret:
+      break
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = haar.detectMultiScale(gray, 1.3, 5)
+    faces = haar.detectMultiScale(gray, 1.3, 3)
     for(x, y, w, h) in faces:
-      #cv2.rectangle(img, (x, y), (x+w, y+h), (175, 244, 65), 1)
       roi_gray = gray[y:y+h, x:x+w]
-      roi_color = img[y:y+h, x:x+w]
       face_id, conf = reg.predict(roi_gray)
-      if face_id == 1:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (244, 196, 65), 3)
-        #cv2.imshow("Chino", roi_color)
-        face_name = "Chino"
-        cv2.putText(img,str(face_name), (x, y), cv2.FONT_HERSHEY_DUPLEX, 2, (244, 196, 65))
-      elif face_id == 2:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (65, 128, 244), 3)
-        #cv2.imshow("Cocoa", roi_color)
-        face_name = "Cocoa"
-        cv2.putText(img,str(face_name), (x, y), cv2.FONT_HERSHEY_DUPLEX, 2, (65, 128, 244))
-      elif face_id == 3:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (244, 65, 143), 3)
-        #cv2.imshow("Rize", roi_color)
-        face_name = "Rize"
-        cv2.putText(img,str(face_name), (x, y), cv2.FONT_HERSHEY_DUPLEX, 2, (244, 65, 143))
-      elif face_id == 4:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (65, 238, 244), 3)
-        #cv2.imshow("Syaro", roi_color)
-        face_name = "Syaro"
-        cv2.putText(img,str(face_name), (x, y), cv2.FONT_HERSHEY_DUPLEX, 2, (65, 238, 244))
-      elif face_id == 5:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (65, 244, 71), 3)
-        #cv2.imshow("Chiya", roi_color)
-        face_name = "Chiya"
-        cv2.putText(img,str(face_name), (x, y), cv2.FONT_HERSHEY_DUPLEX, 2, (65, 244, 71))
-      cv2.imshow("detection with LBP", img)
-      if cv2.waitKey(1) == ord('q'):
-        break
+      for idx, (name, colo_val) in enumerate(face_name.items()):
+        if idx == face_id and conf > 70:
+          cv2.rectangle(img, (x, y), (x+w, y+h), colo_val, 1)
+          cv2.putText(img, f'{name}, {conf:.2}', (x, y), cv2.FONT_HERSHEY_DUPLEX, 1, colo_val)
+    cv2.imshow("detection with LBP", img)
+    if cv2.waitKey(1) == ord('q'):
+      break
+  cap.release()
+  cv2.destroyAllWindows()
+  exit()
 
 if __name__ == "__main__":
   detect_w_lbp()
